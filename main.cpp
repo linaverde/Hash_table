c2d5f3874f03d6c5f9e05dfb57b0fb9a62bdca56
linaverde <p.koroleva1102@gmail.com>
2019-03-24 21:32:08 +1000

    Change main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
+#include "lib_dir/hash_lib.h"
 
 int main() {
+    Hash_table MyTable(42, 6);
+    std::cout << "Размер таблицы: " << MyTable.getSize() << std::endl;
+    MyTable.pringAll();
+    Rec MyRec;
+    MyRec.value = "a_Журнал";
+    //добавление элементов
+    for (unsigned int i = 1 ; i <10; i++) {
+        MyRec.key =10000+ 3*i;
+        MyRec.value[0]++;
+        MyTable.add(MyRec);
+        MyRec.key =10000+ 2*i;
+      //  MyRec.value[0]++;
+        MyTable.add(MyRec);
+    }
+    std::cout << "Размер таблицы: " << MyTable.getSize() << std::endl;
+    MyTable.pringAll();
+    MyRec.value = "a_Журнал";
+    //удаление элементов
+    for (unsigned int i = 1; i < 10; i++) {
+       MyRec.key =10000+ 3*i;
+        MyRec.value[0]++;
+       MyTable.del(MyRec);
+        MyRec.key =10000+ 2*i;
+        //  MyRec.value[0]++;
+      //  MyTable.del(MyRec);
+    }
+    std::cout << "Размер таблицы: " << MyTable.getSize() << std::endl;
+    MyTable.pringAll();
+
+    MyRec.value = "a_Журнал";
+    //добавление элементов
+    for (unsigned int i = 1 ; i <10; i++) {
+        MyRec.key =10000+ 5*i;
+        MyRec.value[0]++;
+        MyTable.add(MyRec);
+        MyRec.key =10000+ 2*i;
+        //  MyRec.value[0]++;
+        MyTable.add(MyRec);
+    }
+    std::cout << "Размер таблицы: " << MyTable.getSize() << std::endl;
+    MyTable.pringAll();
 
 }
